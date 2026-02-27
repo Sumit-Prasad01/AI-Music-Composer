57ce2c576b6e12710dd4fff0feb9e30c6cf64ab9
Sumit prasad <sumitprasadd.wb24@gmail.com>
2026-02-27 15:50:56 +0530

    add music generation components

---
+++ app/utils.py
@@ -0,0 +1,35 @@
+import io
+import music21
+import numpy as np
+from scipy.io.wavfile import write as write_wav
+from synthesizer import Synthesizer, Waveform
+
+
+def note_to_frequncies(note_list):
+    freqs = []
+
+    for note_str in note_list:
+        try:
+            note = music21.note.Note(note_str)
+            freqs.append(note.pitch.frequency)
+
+        except:
+            continue
+
+    return freqs
+
+
+def generate_wav_bytes_fron_notes(notes):
+    synth = Synthesizer(
+        osc1_waveform = Waveform.sine,
+        osc1_volume = 1.0,
+        use_osc2 = False
+        )
+    sample_rate = 44100
+
+    audio = np.concatenate([synth.generate_constant_wave(freq, 0.5) for freq in notes])
+
+    buffer = io.BytesIO()
+    write_wav(buffer, sample_rate, audio.astype(np.float32))
+
+    return buffer.getvalue()
